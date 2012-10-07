d2bd7ffdc404ce67d3553ed7980bdf6f63576c0a
Paul Beyer <plbeyer70@gmail.com>
2012-10-07 13:51:32 -0400

    Configured CloudBees DB

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -23,21 +23,13 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
+	
     production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
+       dataSource {
+           dbCreate = "create-drop"
+           url = "[[jdbc:cloudbees://charity_champ_db]]"
+           username = "charityChamp"
+           password = "FoodBank12"
+       }
     }
 }
