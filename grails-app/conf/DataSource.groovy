8ca9ed71a7c8be8f30904b72b101ebfab9b737b3
Paul Beyer <plbeyer70@gmail.com>
2012-10-08 22:03:57 -0400

    test

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -36,7 +36,7 @@ environments {
     production {
         dataSource {
             dbCreate = ""
-            jndiName = "java:comp/env/jdbc/charityChamp"
+            jndiName = "java:comp/env/charityChamp"
 			
         }
     }
