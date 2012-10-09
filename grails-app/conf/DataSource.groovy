dbda2aefdf7fdf1228de19998dbcdb4399dec41e
Paul Beyer <plbeyer70@gmail.com>
2012-10-08 20:37:02 -0400

    Changes to configure jdbc jndi entry.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,11 +1,5 @@
 dataSource {
-//    pooled = true
-//    driverClassName = "org.h2.Driver"
-//    username = "sa"
-//    password = ""
-	
-	driverClassName = "com.mysql.jdbc.Driver"
-	dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+    pooled = true
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -16,27 +10,33 @@ hibernate {
 environments {
     development {
         dataSource {
-//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-			dbCreate = "create-drop"
-			url = "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com:3306/charity_champ_db"
-			username = "charityChamp"
-			password = "FoodBank12"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
+//			dbCreate = "create-drop"
+//			url = "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com:3306/charity_champ_db"
+//			username = "charityChamp"
+//			password = "FoodBank12"
+//			driverClassName = "com.mysql.jdbc.Driver"
+//			dialect = org.hibernate.dialect.MySQL5InnoDBDialect
         }
     }
     test {
         dataSource {
             dbCreate = "update"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
         }
     }
 	
     production {
-       dataSource {
-           dbCreate = "update"
-           url = "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com:3306/charity_champ_db"
-           username = "charityChamp"
-           password = "FoodBank12"
-       }
+        dataSource {
+            dbCreate = ""
+            jndiName = "jdbc/charityChamp"
+        }
     }
 }
